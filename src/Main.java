6c94a7f280a15f91031a84ccdf0bd9bfaa465229
Dipak Shrestha <shresthadipak2049@gmail.com>
2023-03-23 22:36:42 +0545

    Feat: variables and constant

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
+
+        // Declare the Variables
+        int a = 15;
+        int b = 20;
+        int c;
+        c = a + b;
+        System.out.println(c);
+
+        // Declare the constant
+        final int x = 30;
+        final int y = 40;
+        int z;
+        z = x * y;
+        System.out.println(z);
+
     }
 }
