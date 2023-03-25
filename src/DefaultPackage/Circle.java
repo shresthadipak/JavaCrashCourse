b219eb78af7cb3318856ad9c7871c2c5ff8320d0
Dipak Shrestha <shresthadipak2049@gmail.com>
2023-03-25 15:25:15 +0545

    Feat: working on static members like static methods, importing static library

---
+++ src/DefaultPackage/Circle.java
@@ -0,0 +1,40 @@
+package DefaultPackage;
+
+// static library
+import static java.util.Calendar.MONDAY;
+
+public class Circle {
+
+    // declaration of identifiers
+    static double pi;
+    int radius;
+
+    static{
+        System.out.println("Initializing system....");
+        pi = 3.14;
+    }
+
+    // Constructor
+    public Circle(int radius){
+        this.radius = radius;
+        System.out.println("Inside Constructor");
+    }
+
+    // static method
+    public static double piValue(){
+        return pi;
+    }
+
+    public static void main(String[] args){
+        Circle vObj1 = new Circle(5);
+        Circle vObj2 = new Circle(10);
+        System.out.println("Value of pi: "+vObj2.pi);
+
+        if(MONDAY == 2){
+            System.out.println("Today is Monday");
+        }else{
+            System.out.println("Today is not");
+        }
+    }
+
+}
